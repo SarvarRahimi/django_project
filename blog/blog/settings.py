d166eb0f9a8fe790ec1f010033caee641d933850
SarvarRahimi <sarvarrahimi111@gmail.com>
2021-01-27 12:06:21 +0330

    postgres info added to settings and url.py added to posting

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'posting',
+        'USER': 'postgresql',
+        'PASSWORD': 'sarvar55231647',
+        'HOST': 'localhost'
     }
 }
 
