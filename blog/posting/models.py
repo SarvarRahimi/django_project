4bb02e3d21c7d56de6c66e688250b751839a5533
SarvarRahimi <62295105+SarvarRahimi@users.noreply.github.com>
2021-02-06 18:09:33 +0330

    database modified and app name added to urls and post_extras added

--- blog/posting/models.py
+++ blog/posting/models.py
@@ -1,4 +1,5 @@
 # from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,52 +12,41 @@ class Category(models.Model):
 
 class Label(models.Model):
     label_text = models.CharField(max_length=100, verbose_name='label text', name='')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.label_text
 
 
-class Like(models.Model):
-    like_text = models.CharField(max_length=10, verbose_name='like text', name='')
-    like_count = models.IntegerField(verbose_name='like count', name='')
-    like_date = models.DateTimeField(verbose_name='like date', name='', default='2021-01-28')
+class BlogUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='', name='')
+    email = models.EmailField(verbose_name='email', name='')
+    phone_number = models.IntegerField(verbose_name='phone number', name='')
+    image = models.ImageField(verbose_name='image', name='')
 
     def __str__(self):
-        return self.like_count
+        return f"{self.user}"
 
 
 class Comment(models.Model):
-    like = models.ForeignKey(Like, on_delete=models.CASCADE)
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='', name='')
+    like = models.BooleanField(verbose_name='comment_like', name='', default=False)
     comment_text = models.TextField(verbose_name='comment text', name='')
-    date = models.DateTimeField(verbose_name='comment date', name='', default='2021-01-28')
+    time = models.DateTimeField(verbose_name='comment date', name='', default='2021-01-28')
 
     def __str__(self):
         return self.comment_text
 
 
 class Post(models.Model):
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='', name='')
     label = models.ManyToManyField(Label)
-    category = models.ManyToManyField(Category)
-    like = models.ForeignKey(Like, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='', name='')
+    like = models.BooleanField(verbose_name='comment_like', name='', default=False)
     head = models.CharField(max_length=200, verbose_name='post head')
     body = models.TextField(verbose_name='post body')
-    date = models.DateTimeField(verbose_name='post date', name='', default='2021-01-28')
+    time = models.DateTimeField(verbose_name='post date', name='')
     image = models.ImageField(verbose_name='post image')
 
     def __str__(self):
         main_post = f'{self.head} \n {self.body}'
         return main_post
-
-
-class User(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, verbose_name='first name', name='')
-    last_name = models.CharField(max_length=100, verbose_name='last name', name='')
-    email = models.EmailField(verbose_name='email', name='')
-    phone_number = models.IntegerField(verbose_name='phone number', name='')
-    image = models.ImageField(verbose_name='image', name='')
-
-    def __str__(self):
-        return f"first name:{self.first_name}\nlast name:{self.last_name}\nemail:{self.email}"
