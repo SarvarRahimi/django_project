3eee43a845ee5fcf6fe17406c039d6f373c79ca9
SarvarRahimi <62295105+SarvarRahimi@users.noreply.github.com>
2021-02-10 13:16:12 +0330

    some changes in url models migrations and settings

--- blog/posting/models.py
+++ blog/posting/models.py
@@ -29,7 +29,7 @@ class BlogUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='کاربر')
     email = models.EmailField(verbose_name='ایمیل', name='email')
     phone_number = models.IntegerField(verbose_name='شماره موبایل')
-    image = models.ImageField(verbose_name='عکس')
+    image = models.ImageField(verbose_name='عکس', upload_to='media/user/%Y/%m/%d/', blank=True)
 
     class Meta:
         verbose_name = "کاربر"
@@ -41,9 +41,9 @@ class BlogUser(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='کاربر', null=True)
-    like = models.BooleanField(verbose_name='لایک', default=False)
-    comment_text = models.TextField(verbose_name='عنوان')
+    comment_text = models.TextField(verbose_name='عنوان', editable=False)
     time = models.DateTimeField(verbose_name='زمان', default=timezone.now)
+    like = models.BooleanField(verbose_name='لایک', default=False)
     permission = models.BooleanField(verbose_name='اجازه نمایش', default=False)
     activated = models.BooleanField(verbose_name='وضعیت فعال بودن', default=False)
 
@@ -59,12 +59,13 @@ class Post(models.Model):
     author = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='نویسنده', null=True)
     label = models.ManyToManyField(Label, verbose_name='برچسب')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='دسته بندی', null=True)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, verbose_name='کامنت')
-    like = models.BooleanField(verbose_name='لایک', default=False)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, verbose_name='کامنت', null=True)
     head = models.CharField(max_length=200, verbose_name='تیتر')
     body = models.TextField(verbose_name='متن')
+    summary = models.CharField(max_length=200, verbose_name='خلاصه', blank=True)
     time = models.DateTimeField(verbose_name='زمان', default=timezone.now)
-    image = models.ImageField(verbose_name='عکس')
+    image = models.ImageField(verbose_name='عکس', upload_to='media/%Y/%m/%d/', blank=True)
+    like = models.BooleanField(verbose_name='لایک', default=False)
     permission = models.BooleanField(verbose_name='اجازه نمایش', default=False)
     activated = models.BooleanField(verbose_name='وضعیت فعال بودن', default=False)
 
@@ -75,3 +76,29 @@ class Post(models.Model):
     def __str__(self):
         main_post = f'{self.head} \n {self.body}'
         return main_post
+
+
+class PostLikes(models.Model):
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='کاربر', null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='پست')
+    type = models.BooleanField(verbose_name='نوع')
+
+    class Meta:
+        verbose_name = "لایک پست"
+        verbose_name_plural = "لایک های پست"
+
+    def __str__(self):
+        return f'{self.user} \n {self.post}\n like type: {type}'
+
+
+class CommentLikes(models.Model):
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name='کاربر', null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='پست')
+    type = models.BooleanField(verbose_name='نوع')
+
+    class Meta:
+        verbose_name = "لایک کامنت"
+        verbose_name_plural = "لایک های کامنت"
+
+    def __str__(self):
+        return f'{self.user} \n {self.post}\n like type: {type}'
