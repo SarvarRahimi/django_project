4804b9e019af0e02c4a9b487c93681131d97bd3d
SarvarRahimi <62295105+SarvarRahimi@users.noreply.github.com>
2021-02-15 09:59:40 +0330

    the home page is running

--- blog/posting/views.py
+++ blog/posting/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+from posting.models import Post
 
 
 def showPosts(request):
-    return True
+    ordered_posts = Post.objects.order_by('-time')
+    return render(request, 'posting/showPosts.html', {'posts': ordered_posts})
+
