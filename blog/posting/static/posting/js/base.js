81445a26ef8f5ac92096dec4de44abddb5bcd622
SarvarRahimi <62295105+SarvarRahimi@users.noreply.github.com>
2021-02-16 07:59:38 +0330

    function time since added

--- blog/posting/static/posting/js/base.js
+++ blog/posting/static/posting/js/base.js
@@ -1,37 +1,64 @@
-function timeSince(date) {
-    let pubDiv = document.getElementById("pubDate")
+// function timeSince(date) {
+//     let pubDiv = document.getElementById("pubDate")
+//
+//     const seconds = Math.floor((new Date() - date) / 1000);
+//
+//     let interval = seconds / 31536000;
+//
+//     let result;
+//     if (interval > 1) {
+//         result = Math.floor(interval) + " سال قبل";
+//         pubDiv.innerHTML = result;
+//     }
+//     interval = seconds / 2592000;
+//     if (interval > 1) {
+//         result = Math.floor(interval) + " ماه قبل";
+//         pubDiv.innerHTML = result;
+//     }
+//     interval = seconds / 86400;
+//     if (interval > 1) {
+//         result = Math.floor(interval) + " روز قیل";
+//         pubDiv.innerHTML = result;
+//     }
+//     interval = seconds / 3600;
+//     if (interval > 1) {
+//         result = Math.floor(interval) + " ساعت قبل";
+//         pubDiv.innerHTML = result;
+//     }
+//     interval = seconds / 60;
+//     if (interval > 1) {
+//         result = Math.floor(interval) + " دقیقه قبل";
+//         pubDiv.innerHTML = result;
+//     }
+//     else {
+//         result = Math.floor(seconds) + " ثانیه قبل";
+//         pubDiv.innerHTML = result;
+//     }
+// }
 
-    const seconds = Math.floor((new Date() - date) / 1000);
 
-    let interval = seconds / 31536000;
+$(function () {
+    $("#likeSvg").on("click",function (event) {
+        event.preventDefault();
+        let data = {
+            "user_id": $(this).attr('user-id'),
+            "post_id": $(this).attr('post-id'),
+            "status": "True",
+        };
 
-    let result;
-    if (interval > 1) {
-        result = Math.floor(interval) + " سال قبل";
-        pubDiv.innerHTML = result;
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        result = Math.floor(interval) + " ماه قبل";
-        pubDiv.innerHTML = result;
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        result = Math.floor(interval) + " روز قیل";
-        pubDiv.innerHTML = result;
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        result = Math.floor(interval) + " ساعت قبل";
-        pubDiv.innerHTML = result;
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        result = Math.floor(interval) + " دقیقه قبل";
-        pubDiv.innerHTML = result;
-    }
-    else {
-        result = Math.floor(seconds) + " ثانیه قبل";
-        pubDiv.innerHTML = result;
-    }
-}
+        $.ajax({
+            url: 'http://127.0.0.1:5000/order_final/',
+            data: data,
+            method: "POST",
+        })
+            .done(function (result) {
+                alert("ثبت شد")
+                $(this).css("color", "red");
+                $(this).closest('p').html(result);
+                // document.location = 'http://127.0.0.1:5000/';
+            })
+            .fail(function (result) {
+                alert("ثبت نشد" + result['status'] + result['exception']);
+            });
+    });
+});
--- blog/posting/templatetags/post_extras.py
+++ blog/posting/templatetags/post_extras.py
@@ -1,5 +1,6 @@
 from django import template
-
+from math import floor
+from django.utils import timezone
 register = template.Library()
 
 
@@ -7,3 +8,31 @@ register = template.Library()
 def likeCounter(value, arg):
     return value.filter(type__exact=arg).count()
 
+
+@register.filter(name='timeSince')
+def timeSince(value):
+    print(value - value, 'answer')
+    seconds = floor((timezone.now() - value) / 1000)
+
+    interval = seconds / 31536000
+    if interval > 1:
+        return f'{floor(interval)} سال قبل '
+
+    interval = seconds / 2592000
+    if interval > 1:
+        return f'{floor(interval)} ماه قبل '
+
+    interval = seconds / 86400
+    if interval > 1:
+        return f'{floor(interval)} روز قبل '
+
+    interval = seconds / 3600
+    if interval > 1:
+        return f'{floor(interval)} ساعت قبل '
+
+    interval = seconds / 60
+    if interval > 1:
+        return f'{floor(interval)} دقیقه قبل '
+
+    else:
+        return f'{floor(seconds)}  ثانیه قبل '
